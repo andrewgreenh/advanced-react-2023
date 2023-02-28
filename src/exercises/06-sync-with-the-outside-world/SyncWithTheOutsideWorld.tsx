4c5c882b19cf7d0ca5d83a90a4a24da6616cc2a8
Andreas Roth <andreas.roth@esveo.com>
2023-02-28 16:15:37 +0100

    Exercise 6

--- src/exercises/06-sync-with-the-outside-world/SyncWithTheOutsideWorld.tsx
+++ src/exercises/06-sync-with-the-outside-world/SyncWithTheOutsideWorld.tsx
@@ -1,13 +1,49 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+function useElementDimensions<TElement extends HTMLElement>() {
+  const ref = useRef<TElement>(null);
+
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [initDimensions, setInitDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    let initialized = false;
+
+    const observer = new ResizeObserver(() => {
+      const rect = element.getBoundingClientRect();
+      if (!initialized) {
+        setInitDimensions({ width: rect.width, height: rect.height });
+        initialized = true;
+      }
+
+      setDimensions({ width: rect.width, height: rect.height });
+    });
+
+    observer.observe(element);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return { ref, dimensions };
+}
 
 export function SyncWithTheOutsideWorld() {
-  const ref = useRef<HTMLTextAreaElement>(null);
+  const { ref, dimensions } = useElementDimensions<HTMLDivElement>();
 
   return (
-    <div>
+    <div ref={ref}>
       <h1>Sync with the outside world</h1>
 
-      <textarea ref={ref} />
+      <p>
+        width: {dimensions.width} height: {dimensions.height}
+      </p>
+
+      <textarea />
 
       <button>Reset size</button>
     </div>
