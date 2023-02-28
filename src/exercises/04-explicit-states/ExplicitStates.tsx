46de43e629f7fdcdd4594bcbf049c83853c3be35
Andreas Roth <andreas.roth@esveo.com>
2023-02-28 13:45:35 +0100

    Exercise 4

--- src/exercises/04-explicit-states/ExplicitStates.tsx
+++ src/exercises/04-explicit-states/ExplicitStates.tsx
@@ -1,3 +1,90 @@
+import { useQuery } from "react-query";
+import { satellitesApi } from "../../api/satellites";
+
+// type LoadingState<TResult> =
+//   | { type: "LOADING" }
+//   | { type: "SUCCESS"; data: TResult }
+//   | { type: "ERROR"; error: string };
+
 export function ExplicitStates() {
-  return <h1>Explicit states</h1>;
+  return (
+    <>
+      <SatelliteList />
+      <SatelliteList />
+      <SatelliteList />
+    </>
+  );
 }
+
+function SatelliteList() {
+  const satelliteQuery = useQuery(["satellites"], satellitesApi.getAll);
+
+  return (
+    <>
+      <h1>Explicit states</h1>
+      {satelliteQuery.status === "error" ? (
+        <p>
+          Something went wrong!{" "}
+          <button onClick={() => satelliteQuery.refetch()}>Retry</button>
+        </p>
+      ) : satelliteQuery.status === "loading" ? (
+        "Loading"
+      ) : satelliteQuery.status === "success" ? (
+        <ul>
+          {satelliteQuery.data.map((s) => (
+            <li key={s.id}>{s.name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </>
+  );
+}
+
+// import { useCallback, useEffect, useState } from "react";
+// import { Satellite, satellitesApi } from "../../api/satellites";
+
+// type LoadingState<TResult> =
+//   | { type: "LOADING" }
+//   | { type: "SUCCESS"; data: TResult }
+//   | { type: "ERROR"; error: string };
+
+// export function ExplicitStates() {
+//   const [satelliteState, setSatelliteState] = useState<
+//     LoadingState<Satellite[]>
+//   >({
+//     type: "LOADING",
+//   });
+
+//   const reload = useCallback(async function reload() {
+//     setSatelliteState({ type: "LOADING" });
+//     try {
+//       const satellites = await satellitesApi.getAll();
+//       setSatelliteState({ type: "SUCCESS", data: satellites });
+//     } catch (err) {
+//       setSatelliteState({ type: "ERROR", error: String(err) });
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     reload();
+//   }, [reload]);
+
+//   return (
+//     <>
+//       <h1>Explicit states</h1>
+//       {satelliteState.type === "ERROR" ? (
+//         <p>
+//           Something went wrong! <button onClick={reload}>Retry</button>
+//         </p>
+//       ) : satelliteState.type === "LOADING" ? (
+//         "Loading"
+//       ) : (
+//         <ul>
+//           {satelliteState.data.map((s) => (
+//             <li key={s.id}>{s.name}</li>
+//           ))}
+//         </ul>
+//       )}
+//     </>
+//   );
+// }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { App } from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
 
 // TypeScript examples of inference
