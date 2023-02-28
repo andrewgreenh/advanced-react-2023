5c47d956e3c847984b024a7f6f53f4b190cdf9d7
Andreas Roth <andreas.roth@esveo.com>
2023-02-28 11:21:33 +0100

    TypeScript introduction

--- src/exercises/01-first-steps-with-typescript/FirstStepsWithTypeScript.tsx
+++ src/exercises/01-first-steps-with-typescript/FirstStepsWithTypeScript.tsx
@@ -1,3 +1,45 @@
+import { ComponentProps, useEffect, useState } from "react";
+
 export function FirstStepsWithTypeScript() {
-  return <h1>First steps with TypeScript</h1>;
+  return (
+    <>
+      <h1>First steps with TypeScript</h1>
+      <SimpleCounterButton
+        incrementBy={10}
+        style={{ border: "1px solid red" }}
+      />
+      <RandomNumbers />
+    </>
+  );
+}
+
+console.log(FirstStepsWithTypeScript() === FirstStepsWithTypeScript());
+
+type SimpleCounterButtonProps = ComponentProps<"button"> & {
+  incrementBy: number;
+};
+
+function SimpleCounterButton(props: SimpleCounterButtonProps) {
+  const { incrementBy, ...rest } = props;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <button {...rest} onClick={() => setCounter(counter + props.incrementBy)}>
+      {counter}
+    </button>
+  );
+}
+
+function RandomNumbers() {
+  const [randomNumber, setRandomNumber] = useState<null | number>(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRandomNumber(Math.random());
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <p>{randomNumber ?? "Generating a new number"}</p>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,12 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
 );
+
+// TypeScript examples of inference
+let x: number | string = 5;
+
+x = 10;
+
+x = "test";
+
+function Comp(props: { x: number }) {}
