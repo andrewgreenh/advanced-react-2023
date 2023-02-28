12596ee816574f3eaaeb64f335e0f1fde012a4e1
Andreas Roth <andreas.roth@esveo.com>
2023-02-28 13:45:22 +0100

    Exercise 3

--- src/exercises/03-impossible-state/ImpossibleState.tsx
+++ src/exercises/03-impossible-state/ImpossibleState.tsx
@@ -1,3 +1,62 @@
+import { useState } from "react";
+
+export function calcIsValid(name: string, termsAccepted: boolean) {
+  return name.length > 3 && name.length < 12 && termsAccepted;
+}
+
 export function ImpossibleState() {
-  return <h1>Impossible state</h1>;
+  const [name, setName] = useState("");
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  // const [isValid, setIsValid] = useState(false);
+
+  // console.time("valid calc");
+  const isValid = calcIsValid(name, termsAccepted);
+  // console.timeEnd("valid calc");
+
+  // console.log("Render", name, termsAccepted, isValid);
+
+  // useEffect(() => {
+  // }, [name.length, termsAccepted]);
+
+  return (
+    <>
+      <h1>Impossible state</h1>
+      <input
+        placeholder="name"
+        value={name}
+        onChange={(e) => {
+          const name = e.target.value;
+          setName(name);
+
+          // setIsValid(true);
+        }}
+      />
+      <br />
+      <br />
+
+      <label>
+        <input
+          type="checkbox"
+          checked={termsAccepted}
+          onChange={() => {
+            setTermsAccepted(!termsAccepted);
+          }}
+        />
+        Terms of service accepted
+      </label>
+
+      <br />
+      <br />
+      <button disabled={!isValid}>Save</button>
+      <button
+        onClick={() => {
+          setName("");
+          setTermsAccepted(false);
+        }}
+      >
+        Reset
+      </button>
+    </>
+  );
 }
