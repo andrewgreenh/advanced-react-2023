ecb27337b1f0b4912ae8483627aa0b714fcd3454
Andreas Roth <andreas.roth@esveo.com>
2023-03-01 10:19:37 +0100

    Exercise 9

--- src/exercises/09-layout-components/LayoutComponents.tsx
+++ src/exercises/09-layout-components/LayoutComponents.tsx
@@ -1,3 +1,57 @@
+import { ReactNode, useState } from "react";
+
 export function LayoutComponents() {
-  return <h1>Layout Components</h1>;
+  return (
+    <ContentWithSidebar
+      sidebarContent={SidebarContent()}
+      mainContent={<MainContent />}
+    />
+  );
+}
+
+function SidebarContent() {
+  console.log("SidebarContent rerendered");
+  return <h1>Sidebar!</h1>;
+}
+
+function MainContent() {
+  console.log("MainContent rerendered");
+  return (
+    <PageContent>
+      <h1>Layout Components</h1>
+    </PageContent>
+  );
+}
+
+function PageContent(props: { children: ReactNode }) {
+  return <div className="mx-auto max-w-[700px]">{props.children}</div>;
+}
+
+// const content = <MainContent />;
+
+function ContentWithSidebar(props: {
+  mainContent: ReactNode;
+  sidebarContent: ReactNode;
+}) {
+  const [sidebarWidth, setSidebarWidth] = useState(300);
+  // const divRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div>
+      <input
+        type="range"
+        min={200}
+        max={500}
+        onChange={(e) => {
+          setSidebarWidth(parseInt(e.target.value));
+          // divRef.current!.style.width = e.target.value + "px";
+        }}
+      />
+      {sidebarWidth}
+      <div className="flex items-start gap-4">
+        <div style={{ width: sidebarWidth }}>{props.sidebarContent}</div>
+        <div>{props.mainContent}</div>
+      </div>
+    </div>
+  );
 }
