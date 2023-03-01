cc645d5fbffa05d298d5291c2ffae675234d9624
Andreas Roth <andreas.roth@esveo.com>
2023-03-01 11:25:25 +0100

    Add drag and drop to exercise 9

--- src/exercises/09-layout-components/LayoutComponents.tsx
+++ src/exercises/09-layout-components/LayoutComponents.tsx
@@ -34,7 +34,25 @@ function ContentWithSidebar(props: {
   sidebarContent: ReactNode;
 }) {
   const [sidebarWidth, setSidebarWidth] = useState(300);
-  // const divRef = useRef<HTMLDivElement>(null);
+
+  function onMouseDown(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.preventDefault();
+    const initialWidth = sidebarWidth;
+    const initialX = e.clientX;
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+
+    function onMouseMove(e: MouseEvent) {
+      const newX = e.clientX;
+      const newWidth = initialWidth - initialX + newX;
+      setSidebarWidth(newWidth);
+    }
+    function onMouseUp() {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+  }
 
   return (
     <div>
@@ -42,14 +60,23 @@ function ContentWithSidebar(props: {
         type="range"
         min={200}
         max={500}
+        value={sidebarWidth}
         onChange={(e) => {
           setSidebarWidth(parseInt(e.target.value));
           // divRef.current!.style.width = e.target.value + "px";
         }}
       />
       {sidebarWidth}
-      <div className="flex items-start gap-4">
-        <div style={{ width: sidebarWidth }}>{props.sidebarContent}</div>
+      <div className="flex items-stretch gap-4">
+        <div className="relative" style={{ width: sidebarWidth }}>
+          {props.sidebarContent}
+          <div
+            className="absolute right-[-10px] h-full w-[20px] cursor-col-resize"
+            onMouseDown={onMouseDown}
+          >
+            ↔️
+          </div>
+        </div>
         <div>{props.mainContent}</div>
       </div>
     </div>
